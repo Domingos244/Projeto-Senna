6a4df5f6b9c2a9f6cddc4d273bc5d2fb05b6b9c8
Domingos244 <domingosrodrigo333@gmail.com>
2024-08-12 14:36:28 -0300

    meu primeiro commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let formulario = document.querySelector(".formulario")
+
+function aparecerFormulario() {
+    formulario.computedStyleMap.left = "50%"
+}
+
+function desaparecerFormulario() {
+    formulario.computedStyleMap.left = "50%"
+}
