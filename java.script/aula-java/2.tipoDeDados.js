853c95d1e20f29087b9cd2b68092a9794fc7f945
Domingos244 <domingosrodrigo333@gmail.com>
2024-08-21 18:10:35 -0300

    string

---
+++ java.script/aula-java/2.tipoDeDados.js
@@ -0,0 +1,21 @@
+/*
+1. String -> Textos.
+
+
+
+  const texto1 = "textos com aspas duplas "
+  const texto2 ='textos com aspas simples'
+  const texto3 = `textos com a crase` //template literals ou template string
+
+  console.log(texto1)
+  console.log(texto2)
+  console.log(texto3)
+  */
+
+
+
+const myAge = 30
+
+const myString = `Minha idade é ${myAge}`  //Template literals / Template String
+
+console.log(myString)
---
+++ java.script/aula-java/variaveis.js
@@ -0,0 +1,11 @@
+/*
+variaveis
+
+    let - Pode alterar o valor quanddo quiser
+    const - Valor NÂO pode ser declarado NOVAMENTE
+    var - descontinuado / não usar
+*/
+
+const alunos = 25
+
+console.log(alunos)
