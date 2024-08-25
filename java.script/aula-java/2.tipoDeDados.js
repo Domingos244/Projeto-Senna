70500181dd7b889d3465e165bb1c5184b5fdd1d1
Domingos244 <domingosrodrigo333@gmail.com>
2024-08-25 10:34:14 -0300

    Boleanos

--- java.script/aula-java/2.tipoDeDados.js
+++ java.script/aula-java/2.tipoDeDados.js
@@ -3,7 +3,7 @@ Tipos de Dados -
 
 1. String -> Textos.
 2. numbers -> Números
-
+3. Boolean -> boleano
 
 
   const texto1 = "textos com aspas duplas "
@@ -21,7 +21,7 @@ const myString = `Minha idade é ${myAge}`  //Template literals / Template Strin
 
 console.log(myString)
 
-
+     Numbers
 const number1 = 32 / 2
 const fakeNumber = "32 / 2"
 
@@ -29,9 +29,18 @@ console.log(number1)
 console.log(fakeNumber)
 
 
-/*
+
 * vezes
 / divisão
 + mais
 - menos
+
+     Boleanos
+
+false 
+true
+
+let variavel = "234234"
+01011110000101110010
 */
+ 
