0967d2cee7f89db8db679fb2ad5afad9b34a4487
Domingos244 <domingosrodrigo333@gmail.com>
2024-08-25 09:55:54 -0300

    Dadosjs

--- java.script/aula-java/2.tipoDeDados.js
+++ java.script/aula-java/2.tipoDeDados.js
@@ -1,5 +1,8 @@
 /*
+Tipos de Dados - 
+
 1. String -> Textos.
+2. numbers -> Números
 
 
 
@@ -10,12 +13,25 @@
   console.log(texto1)
   console.log(texto2)
   console.log(texto3)
-  */
-
-
+  
 
 const myAge = 30
 
 const myString = `Minha idade é ${myAge}`  //Template literals / Template String
 
 console.log(myString)
+
+
+const number1 = 32 / 2
+const fakeNumber = "32 / 2"
+
+console.log(number1)
+console.log(fakeNumber)
+
+
+/*
+* vezes
+/ divisão
++ mais
+- menos
+*/
