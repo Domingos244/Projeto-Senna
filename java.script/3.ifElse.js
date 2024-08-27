c7f6f7409dd19ffc85be2225dde7c40c0ce61699
Domingos244 <domingosrodrigo333@gmail.com>
2024-08-27 15:58:55 -0300

    ifElse

---
+++ java.script/3.ifElse.js
@@ -0,0 +1,36 @@
+/*  Controlador de Fluxo 
+     - if(Se) -> Faça ISSO
+     - Else (Se não) -> execulta o ELSE
+
+    Operadores de comparação
+        > maior que
+        < menor que
+        == igual que
+     */
+
+        const notaDoAluno = 3
+        const notaDeCorte = 5
+
+        /*if (notaDoAluno > notaDeCorte){
+            //o que será execultado, se o if for verdadeiro 
+            console.log("Parabéns, você passou de ano")
+        } 
+        if(notaDoAluno < notaDeCorte) {
+            console.log("você foi reprovado")
+        }
+        else {
+            // se o if for falso, ele entra aqui
+           console.log("Você foi reprovado") 
+
+        } 
+           const senhaDoUsuario = 123456
+           const senhaDigitada = 123457
+
+           if(senhaDoUsuario == senhaDigitada) {
+            console.log("Seja-bem vindo, você esta logado (a)")
+           }
+           else{
+            console.log("Senha incorreta")
+           }*/
+
+            
