f74acab1432a26f6aff4e77c52af098450f9c753
Domingos244 <domingosrodrigo333@gmail.com>
2024-08-27 16:20:14 -0300

    Functions

---
+++ java.script/aula-java/4.Functiions.js
@@ -0,0 +1,17 @@
+/* Functions / Funções 
+     Techo de código que é executado
+     SOMENTE quando solicitado (Parente jente boa)
+
+*/
+
+const variavel = "abacatinho"
+
+console.log(variavel)
+
+function nomeNaTela() {
+
+    console.log("Rodrigo")
+
+}
+
+nomeNaTela()
